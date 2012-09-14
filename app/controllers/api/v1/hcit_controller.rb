8a1d0e5d3a50db85cf6e39a2b22b6c8f26a3abd4
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-14 10:00:37 +0700

    use find_by_id in API controller

--- app/controllers/api/v1/hcit_controller.rb
+++ app/controllers/api/v1/hcit_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::HcitController < ApplicationController
 	def ask_user
 		@invite = AskHCIT.new
 		@invite.sender = current_actor
-		@invite.receiver = User.find_by_slug(params[:receiver]).try(:actor)
+		@invite.receiver = User.find_by_id(params[:receiver]).try(:actor)
 		@invite.clothing_item = ClothingItem.find_by_id(params[:clothing_item_id])
 
 		respond_to do |format|
-	      
-	      format.json { 
+
+	      format.json {
 	      	if @invite.save
 	      		render :json =>{:callback => params[:callback], status: :success }
 	      	else
@@ -23,10 +23,11 @@ class Api::V1::HcitController < ApplicationController
 		@invites = AskHCIT.unanswered.find_by_receiver_id(current_actor.id)
 
 		respond_to do |format|
-	      
-	      format.json { 
+
+	      format.json {
 	      	render :json =>@invites,:callback => params[:callback]
 	      }
     	end
 	end
 end
+
