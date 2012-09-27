6b7e6e43ab46747fcfcdcd8e6a281975926835ba
Justin Saul <justins@aplaceformom.com>
2012-09-26 21:40:22 -0700

    fixing badge icon

--- lib/badges.rb
+++ lib/badges.rb
@@ -1,6 +1,13 @@
-badge "Shopholic Lvl 1" do
-  thing PointsUsers
-  subject :user
-  count 3
-
+Badge.class_eval do
+  def self.find_or_create_by_name(name)
+    Badge.where(:name => name).first || create(:name => name)
+  end
+  
+  def icon
+    if read_attribute(:icon)
+    	read_attribute(:icon)
+    else
+      "/images/#{name.parameterize}.jpg"
+    end
+  end
 end
